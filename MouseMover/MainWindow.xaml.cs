4569eabfcc5cd8fae6cb95a51fe69bd9510eecd6
Justin Wing Chung Hui <wingchung@justinhui.co.uk>
2020-10-21 22:28:34 +0100

    First checkin

---
+++ MouseMover/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MouseMover
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Mover _mouseMOver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mouseMOver = new Mover();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            _mouseMOver.Enable();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _mouseMOver.Disable();
+        }
+    }
+}
---
+++ MouseMover/Mover.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using WindowsInput;
+
+namespace MouseMover
+{
+    public class Mover
+    {
+
+        private Timer _timer;
+
+        public Mover()
+        {
+            _timer = new Timer();
+            _timer.Interval = 3000;
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        public void Enable()
+        {
+            _timer.Start();
+        }
+
+        public void Disable()
+        {
+            _timer.Stop();
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var inSim = new InputSimulator();
+            inSim.Mouse.MoveMouseBy(0, 0);
+        }
+    }
+}
